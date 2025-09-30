f35e0669ac695d4402e57fef55fcc4290368ef16
Naveen Kumar P <naveenkumar11202006@gmail.com>
2025-09-30 17:42:25 +0530

    gg

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import HeroSection from './components/HeroSection';
 import CompletedWorkSection from './components/CompletedWorkSection';
 import StartupSection from './components/StartupSection';
-import ReviewsSection from './components/ReviewsSection';
 import WorkWithMeSection from './components/WorkWithMeSection';
 
 function App() {
@@ -11,7 +10,6 @@ function App() {
       <HeroSection />
       <CompletedWorkSection />
       <StartupSection />
-      <ReviewsSection />
       <WorkWithMeSection />
     </div>
   );
